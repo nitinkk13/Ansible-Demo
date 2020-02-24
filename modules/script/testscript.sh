342b5b4a5e9f9375b7261ee5f29ba7387062f38d
Nitin Kumar <nitinchoudhary13@ymail.com>
2020-02-24 21:58:58 +0530

    Complete Ansible Demos

---
+++ modules/script/testscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pwd >> /tmp/filecreatedbyscript.txt
+
+echo "First argument: $1" >> /tmp/filecreatedbyscript.txt
+echo "################************=============#############" >> /tmp/filecreatedbyscript.txt
+
+ifconfig -a >> /tmp/filecreatedbyscript.txt
+
